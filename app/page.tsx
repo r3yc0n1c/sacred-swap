3799342f3354d63578cbed546be77dc59b8854ca
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-06-02 22:22:41 +0530

    inti shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col">
+      <h1 className="text-2xl"> Hello! </h1>
       Welcome to Sacred Swap!!!
     </main>
   );
